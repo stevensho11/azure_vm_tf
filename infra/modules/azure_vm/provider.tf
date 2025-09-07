f502c890c445424d832641eb83c387584858f24e
Stevens Ho <stevensh378@gmail.com>
2025-09-07 15:54:02 -0400

    Changes to azure_vm module

--- infra/modules/azure_vm/provider.tf
+++ infra/modules/azure_vm/provider.tf
@@ -8,7 +8,5 @@ terraform {
 }
 
 provider "azurerm" {
-    features {
-
-    }
+    features {}
 }
---
+++ infra/modules/azure_vm/variables.tf
@@ -0,0 +1,5 @@
+variable "ssh_pub_key" {
+  type = string
+  description = "Public key for VM"
+  sensitive = false
+}
